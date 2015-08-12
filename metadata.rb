b5eb8484e9ef3396748ffc5c0cde269b743b8098
petecarapetyan <pete@couldbe.net>
2015-08-12 10:16:23 -0500

    first working version named correctly before moving to datafundamentals

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'dfdevm_java_eclipse'
+maintainer       'Pete Carapetyan'
+maintainer_email 'pete@datafundamentals.com'
+license          'all_rights'
+description      'Installs/Configures dfdevm_java_eclipse'
+long_description 'Installs/Configures eclipse ee with java and maven'
+version          '0.1.0'
+
+depends 'df_java'
+depends 'df_maven'
+depends 'df_eclipse_ee'
---
+++ recipes/default.rb
@@ -0,0 +1,3 @@
+include_recipe "df_java::default"
+include_recipe "df_maven::default"
+include_recipe "df_eclipse_ee::default"
