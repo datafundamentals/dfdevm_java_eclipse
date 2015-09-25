c570644432849b618331c2e9c70a624d2ab2ab9c
petecarapetyan <pete@couldbe.net>
2015-09-25 07:22:31 -0500

    catch up commit Sept 25 2015

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      'Installs/Configures dfdevm_java_eclipse'
 long_description 'Installs/Configures eclipse ee with java and maven'
 version          '0.1.0'
 
+depends 'df_docker'
 depends 'df_java'
 depends 'df_maven'
 depends 'df_eclipse_ee'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,4 @@ Chef::Log.warn("\n.\n.\n.\n.\n.\n.\n.\nIF YOU DO NOT HAVE A FOLDER NAMED 'shuttl
 include_recipe "df_java::default"
 include_recipe "df_maven::default"
 include_recipe "df_eclipse_ee::default"
+include_recipe "df_docker::default"
